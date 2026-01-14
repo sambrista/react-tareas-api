dc7967ae9847152a586d5aceb568d0e1f0ae213c
Alfonso Jiménez <yo@alfonsojimenez.net>
2026-01-14 13:24:44 +0100

    Añadido botón borrado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -3,19 +3,33 @@ import { taskService } from "../services/taskService";
 import type { Task } from "../types/Task";
 
 function TaskList() {
-    const [tareas, setTareas] = useState<Task[]>([]);
+  const [tareas, setTareas] = useState<Task[]>([]);
+  const [cargando, setCargando] = useState<boolean>(true);
 
-    useEffect(() => {
-        taskService.getAll().then(listaTareas => setTareas(listaTareas));
-    }, []);
+  useEffect(() => {
+    taskService
+      .getAll()
+      .then((listaTareas) => {
+        setTareas(listaTareas);
+      })
+      .finally(() => setCargando(false));
+  }, []);
 
-    return (
+  function borrarTarea(idTarea : number) :void {
+    alert(idTarea);
+  }
+
+  return (
+    <>
+      {cargando && <p>Cargando...</p>}
+      {!cargando && (
         <ul>
-            {tareas && tareas.map(tarea => (
-                <li key={tarea.id}>{tarea.title}</li>
-            ))}
+          {tareas &&
+            tareas.map((tarea) => <li key={tarea.id}>{tarea.title} <button className="edit" onClick={() => borrarTarea(tarea.id)}>❌</button></li>)}
         </ul>
-    )
+      )}
+    </>
+  );
 }
 
 export default TaskList;
